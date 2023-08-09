7c04174b38cf79ec773faaf4de06e9e614e211d4
AJ-Gonzalez <aj.gonzalez.dev@gmail.com>
2023-08-09 02:35:58 -0600

    fix: removed import errors

--- rxgemini/__main__.py
+++ rxgemini/__main__.py
@@ -2,6 +2,8 @@
 
 import typer
 
+from rxgemini import styles
+
 
 app = typer.Typer()
 
@@ -22,11 +24,44 @@ def callback():
 
 
 @app.command()
-def example():
+def about():
+    """
+    About RX Gemini and Licensing
+    """
+    description: str = """
+    RX Gemini: Python unit test automation toolkit
+    Copyright (C) 2023  AJ Gonzalez
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    """
+    typer.echo(description)
+
+
+@app.command()
+def generate_config():
+    """
+    Configuration file Generator
+    """
+    typer.echo(styles.cyan_bold("WIP Placeholder for CFG generator"))
+
+
+@app.command()
+def generate_tests():
     """
     Sample Command
     """
-    typer.echo("example")
+    typer.echo(styles.cyan_bold("WIP Placeholder for test generator"))
 
 
 if __name__ == "__main__":
