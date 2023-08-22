3ccf19a79bbde802d4cd91117f543c2e01238d8c
AJ-Gonzalez <aj.gonzalez.dev@gmail.com>
2023-08-21 18:32:31 -0600

    feat: mvp of logging module done

--- rxgemini/log_handler.py
+++ rxgemini/log_handler.py
@@ -3,14 +3,17 @@
 
 import logging
 from logging.config import dictConfig
+from datetime import datetime
 from typing import Any
 import coloredlogs
 
+import typer
 from rich import print as pprint
 
 from rxgemini.configurator import config_checker
 from rxgemini.constants import LOG_CONF, LOGGING_OPTS
 from rxgemini.errors import ErroneousConfigError
+from rxgemini import styles
 
 
 dictConfig(LOG_CONF)
@@ -32,6 +35,18 @@ if LOG_MODE not in LOGGING_OPTS:
     raise ErroneousConfigError
 
 
+def timestamp() -> str:
+    """
+    Provides formatted timestamp
+
+    Returns:
+        str: Human readable
+    """
+    t_stamp = datetime.now()
+    human_readable: str = str(t_stamp).split(".", maxsplit=1)[0]
+    return human_readable
+
+
 def pretty_print(item: Any):
     """
 
@@ -43,24 +58,68 @@ def pretty_print(item: Any):
     pprint(item)
 
 
-def info(message: str):
+def log_info(message: str):
     if LOG_MODE == LOGGING_OPTS[0]:
         logger.info("%s %s", PREFIX, message)
+    elif LOG_MODE == LOGGING_OPTS[1]:
+        typer.echo(styles.green_bold(message))
+    elif LOG_MODE == LOGGING_OPTS[2]:
+        message = f"{timestamp()}  {message}"
+        typer.echo(styles.green_bold(message))
 
 
-def warning(message: str):
+def general(message: str):
+    if LOG_MODE == LOGGING_OPTS[0]:
+        logger.info("%s %s", PREFIX, message)
+    elif LOG_MODE == LOGGING_OPTS[1]:
+        typer.echo(styles.cyan_bold(message))
+    elif LOG_MODE == LOGGING_OPTS[2]:
+        message = f"{timestamp()}  {message}"
+        typer.echo(styles.cyan_bold(message))
+    else:
+        typer.echo(styles.cyan_bold(message))
+
+
+def log_warning(message: str):
     if LOG_MODE == LOGGING_OPTS[0]:
         logger.warning("%s %s", PREFIX, message)
+    elif LOG_MODE == LOGGING_OPTS[1]:
+        typer.echo(styles.yellow_bold(message))
+    elif LOG_MODE == LOGGING_OPTS[2]:
+        message = f"{timestamp()}  {message}"
+        typer.echo(styles.yellow_bold(message))
+    else:
+        typer.echo(styles.yellow_bold(message))
 
 
-def error(message: str):
+def log_error(message: str):
     if LOG_MODE == LOGGING_OPTS[0]:
         logger.error("%s %s", PREFIX, message)
+    elif LOG_MODE == LOGGING_OPTS[1]:
+        typer.echo(styles.red_bold(message))
+    elif LOG_MODE == LOGGING_OPTS[2]:
+        message = f"{timestamp()}  {message}"
+        typer.echo(styles.red_bold(message))
+    else:
+        typer.echo(styles.red_bold(message))
 
 
-def critical(message: str):
+def log_critical(message: str):
     if LOG_MODE == LOGGING_OPTS[0]:
         logger.critical("%s %s", PREFIX, message)
+    elif LOG_MODE == LOGGING_OPTS[1]:
+        message = f"!!! -> {message}"
+        typer.echo(styles.red_bold(message))
+    elif LOG_MODE == LOGGING_OPTS[2]:
+        message = f"{timestamp()} !!! -> {message}"
+        typer.echo(styles.red_bold(message))
+    else:
+        message = f"!!! -> {message}"
+        typer.echo(styles.red_bold(message))
 
 
-critical("sample message")
+general("sample message")
+log_info("sample message")
+log_warning("sample message")
+log_error("sample message")
+log_critical("sample message")
