1165deff54612204201809bc8a0158a74e1146f2
AJ-Gonzalez <aj.gonzalez.dev@gmail.com>
2023-08-24 23:42:30 -0600

    feat: created class for testing methods

--- test/test_fetcher.py
+++ test/test_fetcher.py
@@ -82,6 +82,84 @@ def mix_arg_kwarg(
     return (sample_arg, sample_other_arg)
 
 
+# Testbench class with above functions as methods:
+
+class SampleMethodsClass:
+    """
+    A class of sample methods to simulate method calls in testing
+    for RX Gemini.
+    """
+
+    def __init__(self):
+        self.sample_const: int = 777
+
+    def no_args(self):
+        """
+        Test function with no args, kwargs, or anything.
+
+        Returns:
+            None: Literally none
+        """
+        return None
+
+    def one_arg(self, sample_arg: Any) -> Any:
+        """
+
+        Test function with one sample positional argument
+
+        Args:
+            sample_arg (Any): Sample positional arg
+
+        Returns:
+            Any : same value
+        """
+        return sample_arg
+
+    def multi_arg(self, sample_arg: str, sample_other_arg: int) -> tuple:
+        """
+
+        Test function with multiple positional arguments
+
+        Args:
+            sample_arg (str): Sample Arg
+            sample_other_arg (int): sample arg
+
+        Returns:
+            _type_: _description_
+        """
+        return (sample_arg, sample_other_arg)
+
+    def multi_kwarg(self,
+                    sample_arg: str = "sample",
+                    sample_other_arg: int = 155) -> tuple:
+        """
+        Test function with multiple kewyword arguments
+
+        Args:
+            sample_arg (str, optional): Sample arg. Defaults to "sample".
+            sample_other_arg (int, optional): Sample arg. Defaults to 155.
+
+        Returns:
+            tuple : sample
+        """
+        return (sample_arg, sample_other_arg)
+
+    def mix_arg_kwarg(self,
+                      sample_arg: str = "sample",
+                      sample_other_arg: int = 155) -> tuple:
+        """
+        Test function with mix positional and keyword arguments
+
+        Args:
+            sample_arg (str, optional): Sample arg. Defaults to "sample".
+            sample_other_arg (int, optional): Sample arg. Defaults to 155.
+
+        Returns:
+            tuple: sample
+        """
+        return (sample_arg, sample_other_arg)
+
+
 class TestFetcher(unittest.TestCase):
     """
     Tests methods/functions from rxgemini/fetcher.py
