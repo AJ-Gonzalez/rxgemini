b8189ff2c247546525a7d50d80fb23c26f5071a6
AJ-Gonzalez <aj.gonzalez.dev@gmail.com>
2023-09-01 01:38:24 -0600

    feat: it works!

--- rxgemini/cls_tools.py
+++ rxgemini/cls_tools.py
@@ -1,6 +1,9 @@
 """Classification and Validation module"""
 
 import math
+import pickle
+
+from rxgemini.log_handler import log_info
 
 
 def index_finder(args_content: dict) -> int:
@@ -19,6 +22,11 @@ def index_finder(args_content: dict) -> int:
     value was stored, the more variations, the more
     complex it is.
 
+    To avoid having to handle exceptions all byte
+    conversions are done with pickle, and made
+    iterable with bytearray, so they can be reduced
+    with set.
+
     Then we list the bytes and get a len attribute.
     After that, we get the median len() of the set
     of parameters.
@@ -34,15 +42,16 @@ def index_finder(args_content: dict) -> int:
     """
     indices = []
     for key, value in args_content.items():
-        bytes_val = bytes(value)
-        set_val = set(bytes_val)
+        log_info(f"Evaluating complexity for: {key}")
+        pickled_bytes = bytearray(pickle.dumps(value))
+        set_val = set(pickled_bytes)
         param_index = len(set_val)
         indices.append(param_index)
-        print(key, param_index)
+        log_info(f"{key} has index: {param_index}")
 
     indices.sort()
     middle_index = len(indices)/2
     exact_middle_index = math.floor(middle_index)
     complexity_index = indices[exact_middle_index]
-    print(complexity_index)
+    log_info(f"Complexity index is: {complexity_index}")
     return complexity_index
