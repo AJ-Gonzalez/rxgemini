9a637b9d798460df328db9aeac04f49e689fe3da
AJ-Gonzalez <aj.gonzalez.dev@gmail.com>
2023-12-08 21:18:15 -0600

    tests: added test boilerplate

---
+++ test/test_injector.py
@@ -0,0 +1,26 @@
+"""Test Module for injector"""
+
+import unittest
+
+from rxgemini.injector import (
+    path_handler_for_tests
+)
+
+
+class TestInjector(unittest.TestCase):
+    """
+    Tests methods/functions from rxgemini/fetcher.py
+
+    Args:
+        unittest (class): Inherits from TestCase
+    """
+
+    def test_path_handler_for_tests(self):
+        """
+        Tests Path handler function
+        """
+        self.assertIsInstance(path_handler_for_tests(), int)
+
+
+if __name__ == "__main__":
+    unittest.main()
